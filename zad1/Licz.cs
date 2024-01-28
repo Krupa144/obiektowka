02cffe0f05ced63994df2873e07c3879fa79e138
Kacper Krupa <kacper.krupa@microsoft.wsei.edu.pl>
2024-01-28 16:24:44 +0100

    zad1

---
+++ zad1/Licz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Licz
+    {
+        private int wartosc;
+
+
+        public void Dodaj(int liczba)
+        {
+            wartosc += liczba;
+
+        }
+
+        public void Odejmij(int liczba)
+        {
+            wartosc -= liczba;
+        }
+
+        public Licz(int z) 
+        {
+            wartosc = z;
+        }
+
+        public void Stan()
+        {
+            Console.WriteLine("wartosc obiektu = " + wartosc);
+        }
+    }
+}
---
+++ zad1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Licz liczba1 = new Licz(2);
+            Licz liczba2 = new Licz(5);
+
+            liczba1.Dodaj(5);
+            liczba1.Stan();
+        }
+    }
+}

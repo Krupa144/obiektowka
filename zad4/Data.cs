38b75e5d0e21bfa5ecc7a009c383ced216c55e25
Kacper Krupa <kacper.krupa@microsoft.wsei.edu.pl>
2024-01-30 01:15:16 +0100

    zad4

---
+++ zad4/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Data
+    {
+
+        private DateTime kalendarz;
+        public Data(int year, int month, int day) 
+        {
+            kalendarz = new DateTime(year, month, day);
+
+        }
+
+        public string GetData1()
+        {
+            return kalendarz.ToString();
+        }
+
+        public void przód()
+        {
+            kalendarz = kalendarz.AddDays(7);   
+        }
+
+        public void tył()
+        {
+            kalendarz = kalendarz.AddDays(-7);
+        }
+
+        
+    }
+}
---
+++ zad4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Data kalendarz = new (currentDateTime.Year, currentDateTime.Month, currentDateTime.Day);
+
+            Console.WriteLine("Bieżąca data: " + kalendarz.GetData1());
+
+            kalendarz.przód();
+            Console.WriteLine("Data po przesunięciu o tydzień w przód: " + kalendarz.GetData1());
+
+            kalendarz.tył();
+            Console.WriteLine("Data po przesunięciu o tydzien" + kalendarz.GetData1());
+        }
+    }
+}
